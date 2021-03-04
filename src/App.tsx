e91fd6c4fb2f8a8f61342d88434dce70f70fb3d7
Moaz-Gamal <moaz120560@gmail.com>
2021-03-04 19:24:48 +0200

    App deploy

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Suspense, lazy } from "react";
 import {
   BrowserRouter as Router,
   Redirect,
@@ -6,16 +6,17 @@ import {
   Switch,
 } from "react-router-dom";
 
-import "./App.scss";
-import CityDetails from "./screens/CityDetails/CityDetails";
-import Layout from "./components/Layout/Layout";
-import Home from "./screens/Home/Home";
-import Login from "./screens/SignUp/Login";
-import Signup from "./screens/SignUp/Signup";
-import EditCities from "./screens/EditCities/EditCities";
-
 import AuthRoute from "./utils/AuthRoute";
 import { usePref } from "./context/UserPrefProvider";
+import "./App.scss";
+import Layout from "./components/Layout/Layout";
+import Login from "./screens/SignUp/Login";
+import Signup from "./screens/SignUp/Signup";
+import Home from "./screens/Home/Home";
+// import CityDetails from "./screens/CityDetails/CityDetails";
+// import EditCities from "./screens/EditCities/EditCities";
+const CityDetails = lazy(() => import("./screens/CityDetails/CityDetails"));
+const EditCities = lazy(() => import("./screens/EditCities/EditCities"));
 
 function App() {
   const { userDetails } = usePref();
@@ -24,17 +25,19 @@ function App() {
     <Router>
       <div className="App">
         <Layout>
-          <Switch>
-            <Route path="/signup">
-              {!userDetails ? <Signup /> : <Redirect to="/" />}
-            </Route>
-            <Route path="/login">
-              {!userDetails ? <Login /> : <Redirect to="/" />}
-            </Route>
-            <AuthRoute path="/edit" component={EditCities} />
-            <AuthRoute path="/details/:city" component={CityDetails} />
-            <AuthRoute path="/" component={Home} />
-          </Switch>
+          <Suspense fallback>
+            <Switch>
+              <Route path="/signup">
+                {!userDetails ? <Signup /> : <Redirect to="/" />}
+              </Route>
+              <Route path="/login">
+                {!userDetails ? <Login /> : <Redirect to="/" />}
+              </Route>
+              <AuthRoute path="/edit" component={EditCities} />
+              <AuthRoute path="/details/:city" component={CityDetails} />
+              <AuthRoute path="/" component={Home} />
+            </Switch>
+          </Suspense>
         </Layout>
       </div>
     </Router>
